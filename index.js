fd7dcdf92a12ac8f199b026b5f4e61b579e7c2b0
Robert Riley <rob_a_riley@yahoo.com>
2018-09-27 18:28:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber (PeopleCount, FName){
+  PeopleCount.push (FName);
   var ReturnLine = "Welcome " + FName + ".  You are number" + PeopleCount.length + " in line.";
   return (ReturnLine);
 }
