9db224cd89c9798e092aef051b6a2c8dce76e643
Robert Riley <rob_a_riley@yahoo.com>
2018-09-27 18:43:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing (katzDeliLine){
   if (katzDeliLine.length > 1){
    var BeingServed = katzDeliLine[0];
    katzDeliLine.shift;
-   return ("Currently serving " + BeingServed + " .");
+   return ("Currently serving " + BeingServed + ".");
   }
   return ("There is nobody waiting to be served!");
 }
